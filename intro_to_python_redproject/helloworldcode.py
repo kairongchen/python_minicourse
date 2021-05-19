ecc335a0e9148a1b310520fc7bc4d00ed5b1adf5
Kairong Chen <krchen@iu.edu>
2021-05-19 15:16:44 -0400

    Add files via upload

---
+++ intro_to_python_redproject/helloworldcode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This is a simple example of hello world.
+
+Output file:
+    myfirstoutput_helloworld.txt
+    
+Kairong Chen
+Last edited: 05/13/2021
+
+"""
+
+outfile = open("myfirstoutput_helloworld.txt", "w")
+
+outfile.write("Hello World! \n Hello from xxx.")
+
+outfile.close()
